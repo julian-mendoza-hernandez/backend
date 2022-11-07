08143970785b790b0c8a64cdabbe63804697564e
Julian Mendoza Hernández <111841380+julian-mendoza-hernandez@users.noreply.github.com>
2022-11-06 20:28:47 -0600

    Consultas MySQL

--- server.js
+++ server.js
@@ -1,29 +1,40 @@
-const express= require('express')
-const messagesRouter= require('./routes/messages')
+const express = require('express') 
+const messagesRouter = require('./routes/messages')
+const usuariosRouter = require('./routes/usuarios')
+const cors = require("cors")
+
 class Server{
     constructor(){
-        this.app= express()
-        this.paths= {
-            messages: "/api/v1/messages"
+      this.app = express()
+      this.paths = {
+        messages:"/api/v1/messages",
+        usuarios:"/api/v1/usuarios"
         }
-        this.routes()
+      this.middlewares() 
+      this.routes()
+      
+
     }
 
-    routes(){
-        //this.app.get('/', (req,res)=>{
-            // res.sent("Hello World")
-        //}) End point
+routes(){
     
-        this.app.use(this.paths.messages, messagesRouter)
-    }
+  //this.app.get('/', (req, res) => {
+    //res.send('Hello World')
+    //  }
+  this.app.use(this.paths.messages, messagesRouter)
+  this.app.use(this.paths.usuarios, usuariosRouter)
+}
 
-    listen(){
-        this.app.listen(process.env.PORT,() => {
-            //console.log("Backend en ejecucion en el puerto", process.env.PORT)
-            console.log(process.env.PORT)
-        })
-    }
-    
+  middlewares(){
+    this.app.use(cors()) //Perimiten solicitudes de origen cruzado//
+    this.app.use(express.json()) //Habilita la lectura de contenido en formato JSON//
+  }
+
+  listen(){
+      this.app.listen(process.env.PORT,() => { //Poner un this aqui xd//
+      console.log("Backend en ejecución en el puerto", process.env.PORT)
+      })
+  }
 }
 
 module.exports = Server
