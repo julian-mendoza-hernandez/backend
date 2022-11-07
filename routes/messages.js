4b137c3411dd0446c1bbdc0defc75a3e99a8d5f5
Julian Mendoza Hernández <111841380+julian-mendoza-hernandez@users.noreply.github.com>
2022-11-06 20:53:25 -0600

    Add files via upload

--- routes/messages.js
+++ routes/messages.js
@@ -1,13 +1,22 @@
-const {Router}= require("express")
-const router= Router()
+const {Router} = require("express")
+const router = Router()
+const {
+    rootMessage, 
+    hiMessage, 
+    byeMessage,
+    postMessage,
+    putMessage,
+    deleteMessage
+} = require('../controllers/messages.js')
 
-const {rootMessage, hiMessage, byeMessage, postMessage, putMessage, deleteMessage} = require('../controllers/messages')
+//router.get("",()=>{})
+router.get("/", rootMessage)
 
-router.get("/", rootMessage) //endpoint
-router.get("/hi", hiMessage) 
+router.get("/hi/:name", hiMessage)
+
 router.get("/bye", byeMessage)
-router.post("/", postMessage) 
-router.put("/", putMessage) 
-router.delete("/", deleteMessage) 
 
-module.exports=router
+router.post('/', postMessage)
+router.put('/', putMessage)
+router.delete('/', deleteMessage)
+module.exports = router
